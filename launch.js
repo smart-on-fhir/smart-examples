03a59234681d9a084f5d392d9e36aea385716df3
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-09-12 22:25:12 -0700

    Added growth chart, bp centiles

---
+++ launch.js
@@ -0,0 +1,52 @@
+var fhirServiceUrl = getParameterByName("fhirServiceUrl");
+
+var client = {
+  "client_name": "SMART BP Centiles",
+  "client_uri": "http://smartplatforms.org/smart-app-gallery/bp-centiles/",
+  "logo_uri": "http://vectorblog.org/wp-content/uploads/2012/09/BP-Centiles-screengrab-300x211.jpg",
+  "contacts": [ "info@smartplatforms.org" ],
+  "redirect_uris": [ relative("index.html")],
+  "response_types": ["token"],
+  "grant_types": ["implicit"],
+  "token_endpoint_auth_method": "none",
+  "scope":  "summary search"
+};
+
+BBClient.providers(["https://bbplus-static-registry.aws.af.cm/"], function(providers){
+
+  var matched;
+
+  var matching = providers.filter(function(p){
+    return (p.bb_api.fhir_service_uri === fhirServiceUrl);
+  });
+
+  console.log(matching);
+  if (matching.length === 1) {
+    matched = matching[0];
+  } else if (matching.length === 0) {
+    matched = BBClient.noAuthFhirProvider(fhirServiceUrl);
+  } else {
+    throw "Found >1 match for " + fhirServiceUrl;
+  }
+
+  BBClient.authorize({
+    client: client, 
+    provider: matched,
+    patientId: getParameterByName("patientId")
+  });
+});
+
+function relative(url){
+  return (window.location.origin + window.location.pathname).match(/(.*\/)[^\/]*/)[1] + url;
+}
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
